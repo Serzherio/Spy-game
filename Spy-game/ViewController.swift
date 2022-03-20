8e0984280f093e2e8b05be89e9dc0f5e94c7a8df
Federico <Serzherio@yandex.ru>
2022-03-20 18:07:15 +0300

    Initial Commit

---
+++ Spy-game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spy-game
+//
+//  Created by Сергей on 20.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
