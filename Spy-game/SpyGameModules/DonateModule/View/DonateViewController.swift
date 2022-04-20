5873188ba3131e89e713a7b7f095f3d40285f99a
Federico <Serzherio@yandex.ru>
2022-04-20 21:01:49 +0300

    Ready to AppStore

--- Spy-game/SpyGameModules/DonateModule/View/DonateViewController.swift
+++ Spy-game/SpyGameModules/DonateModule/View/DonateViewController.swift
@@ -9,12 +9,42 @@ import Foundation
 
 class DonateViewController: UIViewController {
     
+    let descriptionTitle = UILabel(textLabel: "Для поддержки автора напишите свои пожелания под игрой в магазине и оцените игру. Благодаря вашим словам, игра получит развитие и поддержку!", font: .noteworthy30())
+    let stopGameButton = UIButton(title: "Вернуться", titleColor: .black, backgroundColor: .white, font: .noteworthy30(), isShadow: true, cornerRadius: 40)
+    
     var presenter: DonatePresenterProtocol?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 213/255, green: 46/255, blue: 46/255, alpha: 1)
+        descriptionTitle.numberOfLines = 0
+        descriptionTitle.textAlignment = .center
+        stopGameButton.addTarget(self, action: #selector(stopGameButtonPressed), for: .touchUpInside)
+        layout()
     }
+    @objc private func stopGameButtonPressed() {
+        presenter?.closeVC()
+    }
+    
+    
+    private func layout() {
+        stopGameButton.translatesAutoresizingMaskIntoConstraints = false
+        descriptionTitle.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stopGameButton)
+        view.addSubview(descriptionTitle)
+        NSLayoutConstraint.activate([
+            descriptionTitle.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            descriptionTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            descriptionTitle.widthAnchor.constraint(equalToConstant: view.layer.frame.width * 0.95),
+            stopGameButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            stopGameButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            stopGameButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30),
+            stopGameButton.heightAnchor.constraint(equalToConstant: 80)
+        ])
+    }
+    
+    
+    
     
 }
 
